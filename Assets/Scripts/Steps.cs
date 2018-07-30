c5637ec7de181d50f4fb0d36f72d2986227041d5
JuliaBerkouwer <julia.berkouwer@hotmail.nl>
2018-07-30 22:40:19 +0200

    Particles
    added a gun and particle effects

---
+++ Assets/Scripts/Steps.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public class Steps : MonoBehaviour {
+
+    public ParticleSystem ps;
+    private Vector3 lastLocation;
+    private CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update ()
+    {
+        if (lastLocation != transform.position && cc.isGrounded)
+        {
+            ps.Play();
+        }
+        lastLocation = transform.position;
+	}
+}
