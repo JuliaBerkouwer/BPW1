7d13745c0b9910521b03579e212f904e21beb37d
JuliaBerkouwer <julia.berkouwer@hotmail.nl>
2018-07-31 18:41:41 +0200

    Timer
    added a timer, endpoint

--- Assets/Scripts/Steps.cs
+++ Assets/Scripts/Steps.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class Steps : MonoBehaviour {
 
@@ -7,17 +7,27 @@ public class Steps : MonoBehaviour {
     private Vector3 lastLocation;
     private CharacterController cc;
 
+    private Timer t;
+
     private void Start()
     {
+        t = GetComponent<Timer>();
         cc = GetComponent<CharacterController>();
     }
 
     void Update ()
     {
-        if (lastLocation != transform.position && cc.isGrounded)
+        if (lastLocation.x != transform.position.x && lastLocation.z != transform.position.z && cc.isGrounded)
         {
+            t.enabled = true;
             ps.Play();
         }
+
+        if (lastLocation.y < -25f)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
         lastLocation = transform.position;
+
+
 	}
 }
